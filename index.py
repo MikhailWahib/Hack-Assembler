5ff1384ff637e4ca7fa070bfb985a3edda7b88de
MikhailWahib <mikhailwahib1@gmail.com>
2024-03-18 01:27:50 +0200

    Add Parser

---
+++ index.py
@@ -0,0 +1,5 @@
+from parser import Parser
+
+p = Parser("D:/Peter/Nand to tetris pt1/nand2tetris/projects/06/add/Add.asm")
+
+p.showCommands()
---
+++ parser.py
@@ -0,0 +1,66 @@
+class Parser:
+    def __init__(self, path):
+        # open asm file
+        self.f = open(path, 'r')
+        self.commands = self.f.readlines()
+        self.cleanedCommands = []
+
+        # clean commands
+        self.cleanCommands()
+
+    def cleanCommands(self) -> None:
+        for command in self.commands:
+            if command[:2] == '//' or command[0] == '\n':
+                continue
+            else:
+                self.cleanedCommands.append(command)
+
+    def hasMoreCommands(self) -> bool:
+        return len(self.cleanedCommands) > 0
+
+    def advance(self) -> None:
+        self.cleanedCommands.pop(0)
+
+    def commandType(self) -> str:
+        if self.cleanedCommands[0][0] == '@':
+            return 'A_COMMAND'
+        elif self.cleanedCommands[0][0] == '(':
+            return 'L_COMMAND'
+        else:
+            return 'C_COMMAND'
+
+    def symbol(self) -> str | None:
+        if self.commandType() == 'A_COMMAND':
+            return self.cleanedCommands[0][1:]
+        elif self.commandType() == 'L_COMMAND':
+            return self.cleanedCommands[0][1:-1]
+        else:
+            return None
+
+    def dest(self) -> str | None:
+        if self.commandType() == 'C_COMMAND':
+            endIdx = self.cleanedCommands[0].find('=')
+            if endIdx == -1:
+                return None
+
+            return self.cleanedCommands[0][:endIdx]
+
+    def comp(self) -> str | None:
+        if self.commandType() == 'C_COMMAND':
+            endIdx = self.cleanedCommands[0].find(';')
+            if endIdx == -1:
+                return None
+
+            return self.cleanedCommands[0][endIdx+1:]
+
+    def jump(self) -> str | None:
+        if self.commandType() == 'C_COMMAND':
+            startIdx = self.cleanedCommands[0].find(';')
+            if startIdx == -1:
+                return None
+
+            return self.cleanedCommands[0][:startIdx]
+
+    def showCommands(self) -> None:
+        print(f"\n Original Commands: {self.commands} \n")
+        print(f"Cleaned Commands: {self.cleanedCommands} \n")
