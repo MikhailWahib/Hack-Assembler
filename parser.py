108cc0abdb37f6ca4d35b13929c9defef75badd7
MikhailWahib <mikhailwahib1@gmail.com>
2024-03-18 05:05:11 +0200

    Fix parser

--- parser.py
+++ parser.py
@@ -14,6 +14,7 @@ class Parser:
             if command[:2] == '//' or command[0] == '\n':
                 continue
             else:
+                command = command.replace(' ', '')
                 self.cleanedCommands.append(command)
 
     def hasMoreCommands(self):
